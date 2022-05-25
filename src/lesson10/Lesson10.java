6f74b646b212cd7c4b9f9078021d1de99c9b93f4
aleksandr <abakhtinov.nsk@gmail.com>
2022-05-25 12:14:46 +0300

    копия

---
+++ src/lesson10/Lesson10.java
@@ -0,0 +1,26 @@
+package lesson10;
+
+public class Lesson10 {
+
+    public static void main(String[] args) {
+
+        Author tolkin = new Author("Джон", "Толкин");
+
+        Author joanne = new Author("Джоан", "Роулинг");
+
+        Book lor = new Book("Властелин колец", tolkin, 1954 );
+        System.out.println("lor.getAuthor() = " + lor.getAuthor().getName() + " " + lor.getAuthor().getSurname());
+        System.out.println("lor.name = " + lor.getNamebook());
+        System.out.println("lor.publicationYear = " + lor.getPublicationYear());
+
+
+        Book gp = new Book("Гарри Поттер", joanne ,1997);
+        System.out.println("gp.getAuthor() = " + gp.getAuthor().getName() + " " + gp.getAuthor().getSurname());
+        System.out.println("gp.namebook = " + gp.getNamebook());
+        System.out.println("gp.publicationYear = " + gp.getPublicationYear());
+        gp.setPublicationYear(1998);
+        System.out.println("gp.getPublicationYear() = " + gp.getPublicationYear());
+
+    }
+
+}
